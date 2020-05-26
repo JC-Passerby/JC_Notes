7149f53b8a15f5f1fba2ef8603c24d76ffc8f9b9
liujc <506551555@qq.com>
2020-05-26 15:29:55 +0800

    Initial Commit

---
+++ JC_Notes/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  JC_Notes
+//
+//  Created by 刘某某 on 2020/5/26.
+//  Copyright © 2020 刘某某. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ JC_Notes/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  JC_Notes
+//
+//  Created by 刘某某 on 2020/5/26.
+//  Copyright © 2020 刘某某. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
